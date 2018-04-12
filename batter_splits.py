bdd45bce527512bc28c4b1b2db4c895036ec04d5
Jeffrey Zhang <fuzzboy42@gmail.com>
2018-04-11 22:51:51 -0400

    Create list of teams

--- batter_splits.py
+++ batter_splits.py
@@ -1,3 +1,14 @@
 import requests
-req = requests.get('http://api.sportradar.us/mlb/trial/v6.5/en/seasons/2018/REG/teams/aa34e0ed-f342-4ec6-b774-c79b47b60e2d/splits.json?api_key=yn8ctwj7h8nssg3tdyagskfe')
-print(req.json())
+req = requests.get('http://api.sportradar.us/mlb/trial/v6.5/en/seasons/2017/REG/standings.json?api_key=yn8ctwj7h8nssg3tdyagskfe')
+#print(req.json())
+
+data = req.json()
+list_of_teams = []
+#print(leagues)
+for league in data['league']['season']['leagues']:
+	for division in league['divisions']:
+		for team in division['teams']:
+			#print(team)
+			list_of_teams.append(team)
+
+print('List of teams: {0}'.format(list_of_teams))
